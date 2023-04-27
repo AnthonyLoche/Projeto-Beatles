c1f44923c09477afba92e6afcec6f2354ea4bad9
AnthonyLoche <anthonylocheifc@gmail.com>
2023-04-27 13:51:28 -0300

    COmeço de responisvidade

--- TheBeatles.js
+++ TheBeatles.js
@@ -1,5 +1,46 @@
 
 
+
+
+
+
+
+const yellow = document.querySelector('.yellow');
+const max = document.querySelector('.max')
+
+
+const jump = () => {
+  yellow.classList.add('jump');
+  setTimeout(() => {
+    yellow.classList.remove('jump');
+  }, 500);
+}
+
+const loop = setInterval(() =>{
+
+const maxPosition = max.offsetLeft;
+const yellowPosition = +window.getComputedStyle(yellow).bottom.replace('px', '');
+
+if (maxPosition <= 120 && maxPosition > 0 && yellowPosition < 80 ){
+
+  max.style.animation = 'none';
+  max.style.left = `${maxPosition}px`;
+
+  yellow.style.animation = 'none';
+  yellow.style.bottom = `${yellowPosition}px`;
+
+  yellow.src = "Imagens/subyellowbroken.png";
+  yellow.style.marginbottom = '50px'
+
+  clearInterval('loop')
+}
+
+}, 10 )
+
+document.addEventListener('keydown', jump);
+
+
+
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
 }
@@ -7,8 +48,4 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   
-}
-
-function offsetLeft() {
-
 }
